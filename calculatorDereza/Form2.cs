440560e8d1fbfda1c090ea25f385e5a75bb2810b
Newsch00ler <86545418+Newsch00ler@users.noreply.github.com>
2021-10-26 10:09:21 +0800

    Сохранил введенные на форме 2 данные

--- calculatorDereza/Form2.cs
+++ calculatorDereza/Form2.cs
@@ -15,8 +15,60 @@ namespace calculatorDereza
         public Form2()
         {
             InitializeComponent();
+            textNumerator1.Text = Properties.Settings.Default.numerator.ToString();
+            textDenominator1.Text = Properties.Settings.Default.denominator.ToString();
+            textNumerator2.Text = Properties.Settings.Default.numerator.ToString();
+            textDenominator2.Text = Properties.Settings.Default.denominator.ToString();
+            comboCheck.SelectedItem = Properties.Settings.Default.sign.ToString();
         }        
         
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fraction num1 = new Fraction();
+            Fraction num2 = new Fraction();
+            string sign = comboCheck.Text;
+            try
+            {
+                num1.numerator = int.Parse(this.textNumerator1.Text);
+                num1.denominator = int.Parse(this.textDenominator1.Text);
+                num2.numerator = int.Parse(this.textNumerator2.Text);
+                num2.denominator = int.Parse(this.textDenominator2.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Некорректный ввод", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textAns.Text = "";
+                return;
+            }
+            if (num1.numerator >= num1.denominator || num2.numerator >= num2.denominator)
+            {
+                MessageBox.Show("Неправильная дробь", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textAns.Text = "";
+                return;
+            }
+            else if (comboCheck.SelectedItem == null){
+                MessageBox.Show("Неправильный знак", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                Properties.Settings.Default.numerator = (int)num1.numerator;
+                Properties.Settings.Default.denominator = (int)num1.denominator;
+                Properties.Settings.Default.numerator = (int)num2.numerator;
+                Properties.Settings.Default.denominator = (int)num2.denominator;
+                Properties.Settings.Default.sign = sign;
+                Properties.Settings.Default.Save();
+                textAns.Text = num1.GetSum(num1, num2, sign);
+            }
+        }
+    }
+    /*public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             Fraction num1 = new Fraction();
@@ -46,5 +98,5 @@ namespace calculatorDereza
                 textAns.Text = num1.GetSum(num1, num2, sign);
             }
         }
-    }
+    }*/
 }
